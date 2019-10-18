37b42ea05ff28ba8e9c0920452a1416f9b83affb
tomdonohue1 <56651932+tomdonohue1@users.noreply.github.com>
2019-10-17 20:57:29 -0400

    Update Box3_-_Pump_and_Dump.ino

--- Box3_-_Pump_and_Dump/Box3_-_Pump_and_Dump.ino
+++ Box3_-_Pump_and_Dump/Box3_-_Pump_and_Dump.ino
@@ -1,9 +1,30 @@
+
+#include<Wire.h>
+
+str inputVar = String(2);
+char subSystem = 'x';
+int subChannel = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(3); // I2C assignment
+  Wire.onReceive(receiveEvent);
 
 }
 
+void receiveEvent () //Master has told this unit to do something
+{
+inputVar = Wire.read();
+subSystem = inputVar[0];
+subChannel = inputVar[1];
+
+
+
+}
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+delay(100);
 
 }
